302cac03512343b66a8922846bafd18648bdba49
Dmitry Rafikov <vladulasov366@gmail.com>
2023-07-27 23:53:14 +0600

    added enitites

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const Router = require('express');
 const router = new Router();
 
 const userRouter = require('./userRouter');
+const formRouter = require('./formRouter');
 
 router.use('/user', userRouter);
+router.use('/form', formRouter);
 
 module.exports = router;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,8 +3,8 @@ const router = new Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
 
-router.get('/auth', userController.checkAuth);
+router.get('/auth', authMiddleware, userController.checkAuth);
 router.post('/login', userController.login);
-router.post('/registration', authMiddleware, userController.registration);
+router.post('/registration', userController.registration);
 
 module.exports = router;
